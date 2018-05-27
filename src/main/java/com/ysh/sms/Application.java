bc096d82cf032e9074e5a63e8ba9fb8b75785048
Reasonless <ysh.c@foxmail.com>
2018-05-27 19:16:19 +0800

    名字更改

--- src/main/java/com/ysh/sms/Application.java
+++ src/main/java/com/ysh/sms/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author joey
+ * @author yangshenghong
  */
 @SpringBootApplication
 public class Application {
